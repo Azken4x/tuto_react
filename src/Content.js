c8e7789d1b374f81092772f48cd8a2282b061c8a
Azken4x <moneymik3z@gmail.com>
2023-09-09 17:31:41 -0400

    --- Auto Git Commit ---

--- src/Content.js
+++ src/Content.js
@@ -4,33 +4,8 @@ import React from "react";
 const Content = () => {
   const [name, setName] = useState("Mike");
   const [count, setCount] = useState(0);
-
-  const handleNameChange = () => {
-    const names = ["Mike", "Kevin", "Bob"];
-    const int = Math.floor(Math.random() * 3);
-    setName(names[int]);
-  };
-
-  const handleClick = () => {
-    setCount(count + 1);
-    console.log(count);
-  };
-  const handleClick2 = (name) => {
-    console.log(`${name} clicked it`);
-  };
-  const handleClick3 = (e) => {
-    console.log(e.target.innerText);
-  };
-
-  return (
-    <main>
-      <p onDoubleClick={handleNameChange}>Hello {name}!</p>
-      <button onClick={handleClick}>Click me!</button>
-      <button onClick={(e) => handleClick3(e)}>Click me!</button>
-    </main>
-  );
 };
 
-export default Content;
+return <main></main>;
 
-//1.01.35
+export default Content;
