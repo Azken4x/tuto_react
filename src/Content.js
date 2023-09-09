3486d8f3c29acd2981011a2729fab2a1aac8a161
Azken4x <moneymik3z@gmail.com>
2023-09-09 11:51:36 -0400

    Repo setup

---
+++ src/Content.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Content = () => {
+  const handleNameChange = () => {
+    const names = ["Mike", "Kevin", "Bob"];
+    const int = Math.floor(Math.random() * 3);
+    return names[int];
+  };
+
+  const handleClick = () => {
+    console.log("You clicked it");
+  };
+  const handleClick2 = (name) => {
+    console.log(`${name} clicked it`);
+  };
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText);
+  };
+
+  return (
+    <main>
+      <p onDoubleClick={handleClick}>Hello {handleNameChange()}!</p>
+      <button onClick={handleClick}>Click me!</button>
+      <button onClick={(e) => handleClick3(e)}>Click me!</button>
+    </main>
+  );
+};
+
+export default Content;
